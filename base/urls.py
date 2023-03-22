8111144427962217444269b9ac9e752131677cde
muthosamuel <samuelmwangim98@gmail.com>
2023-03-22 12:50:16 +0300

    main

---
+++ base/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name = "base"
+urlpatterns = [
+    path("", views.index, name='index'),
+    path("book", views.book, name="book"),
+    path("pay/<int:pk>", views.pay, name="pay"),
+    path("payment", views.handle_pay, name="handle_pay"),
+
+]
+
+
+admin.site.site_header = 'Royal Palace Hotel'                    # default: "Django Administration"
+admin.site.index_title = 'Admin panel'                 # default: "Site administration"
+admin.site.site_title = 'Royal Palace Hotel' # default: "Django site admin"
